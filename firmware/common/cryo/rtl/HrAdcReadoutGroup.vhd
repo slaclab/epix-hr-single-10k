bb295ba4d4e19a3a6c94504fddd29464467bc727
Dionisio Doering <ddoering@slac.stanford.edu>
2018-07-06 11:03:28 -0700

    initial commit for Hr12bitADC data receiver firmware

---
+++ firmware/common/cryo/rtl/HrAdcReadoutGroup.vhd
@@ -0,0 +1,101 @@
+-------------------------------------------------------------------------------
+-- File       : HrAdcReadoutGroup.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2016-05-26
+-- Last update: 2018-07-06
+-------------------------------------------------------------------------------
+-- Description:
+-- ADC Readout Controller
+-- Receives ADC Data from a custom high rate adc asic.
+-- Designed specifically for Ultrascale series FPGAs
+-------------------------------------------------------------------------------
+-- This file is part of 'SLAC Firmware Standard Library'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'SLAC Firmware Standard Library', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+library UNISIM;
+use UNISIM.vcomponents.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.HrAdcPkg.all;
+
+entity HrAdcReadoutGroup is
+   generic (
+      TPD_G             : time                 := 1 ns;
+      NUM_CHANNELS_G    : natural range 1 to 8 := 8;
+      DATA_TYPE_G       : string               := "12b14b";
+      IODELAY_GROUP_G   : string               := "DEFAULT_GROUP";
+      XIL_DEVICE_G      : string               := "ULTRASCALE";  -- place
+                                                                 -- holder for
+                                                                 -- an option  "7SERIES"
+      IDELAYCTRL_FREQ_G : real                 := 200.0;
+      DEFAULT_DELAY_G   : slv(8 downto 0)      := (others => '0');
+      ADC_INVERT_CH_G   : slv(7 downto 0)      := "00000000");
+   port (
+      -- Master system clock, 125Mhz
+      axilClk : in sl;
+      axilRst : in sl;
+
+      -- Axi Interface
+      axilWriteMaster : in  AxiLiteWriteMasterType;
+      axilWriteSlave  : out AxiLiteWriteSlaveType;
+      axilReadMaster  : in  AxiLiteReadMasterType;
+      axilReadSlave   : out AxiLiteReadSlaveType;
+
+      -- Reset for adc deserializer
+      adcClkRst : in sl;
+
+      -- Serial Data from ADC
+      adcSerial : in HrAdcSerialGroupType;
+
+      -- Deserialized ADC Data
+      adcStreamClk : in  sl;
+      adcStreams   : out AxiStreamMasterArray(NUM_CHANNELS_G-1 downto 0) :=
+      (others => axiStreamMasterInit((false, 2, 8, 0, TKEEP_NORMAL_C, 0, TUSER_NORMAL_C))));
+end HrAdcReadoutGroup;
+
+-- Define architecture
+architecture rtl of HrAdcReadoutGroup is
+
+   signal idelayCtrlRdy : sl;
+   signal cmt_locked    : sl;
+
+begin
+
+ GEN_ULTRASCALE_HRADC : if ((XIL_DEVICE_G = "ULTRASCALE") and (DATA_TYPE_G = "12b14b")) generate
+    U_HrADC_0 : entity work.Hr12bAdcReadoutGroupUS
+      generic map (
+        TPD_G             => TPD_G,
+        NUM_CHANNELS_G    => NUM_CHANNELS_G,
+        IODELAY_GROUP_G   => IODELAY_GROUP_G,
+        IDELAYCTRL_FREQ_G => IDELAYCTRL_FREQ_G,
+        DEFAULT_DELAY_G   => DEFAULT_DELAY_G,
+        ADC_INVERT_CH_G   => ADC_INVERT_CH_G
+        )
+      port map (
+        axilClk           => axilClk,
+        axilRst           => axilRst,
+        axilReadMaster    => axilReadMaster,
+        axilReadSlave     => axilReadSlave,
+        axilWriteMaster   => axilWriteMaster,
+        axilWriteSlave    => axilWriteSlave,
+        adcClkRst         => adcClkRst,
+        adcSerial         => adcSerial,
+        adcStreamClk      => adcStreamClk,
+        adcStreams        => adcStreams
+        );
+  end generate GEN_ULTRASCALE_HR_ADC;
+end rtl;
+
---
+++ firmware/common/cryo/tb/cryo_tb.vhd
@@ -0,0 +1,284 @@
+-------------------------------------------------------------------------------
+-- Title      : Testbench for design "AD9249ClkUS"
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : cryo_tb.vhd
+-- Author     : Dionisio Doering  <ddoering@tid-pc94280.slac.stanford.edu>
+-- Company    : 
+-- Created    : 2017-05-22
+-- Last update: 2018-07-06
+-- Platform   : 
+-- Standard   : VHDL'87
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2017 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_textio.all;
+
+library STD;
+use STD.textio.all;      
+
+use work.all;
+use work.StdRtlPkg.all;
+
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+
+use work.StdRtlPkg.all;
+use work.AxiStreamPkg.all;
+use work.EpixHrCorePkg.all;
+use work.AxiLitePkg.all;
+use work.AxiPkg.all;
+use work.Pgp2bPkg.all;
+use work.SsiPkg.all;
+use work.SsiCmdMasterPkg.all;
+use work.Ad9249Pkg.all;
+use work.Code8b10bPkg.all;
+
+use work.AppPkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+-------------------------------------------------------------------------------
+
+entity cryo_tb is
+
+end cryo_tb;
+
+-------------------------------------------------------------------------------
+
+architecture cryo_tb_arch of cryo_tb is
+
+  -- component generics
+  constant TPD_G : time := 1 ns;
+  constant NUM_CHANNELS_G : integer := 8;
+  constant IDLE_PATTERN_C : slv(11 downto 0) := x"03F";
+
+  --file definitions
+  constant DATA_BITS   : natural := 12;
+  constant DEPTH_C     : natural := 1024;
+  constant FILENAME_C  : string  := "/afs/slac.stanford.edu/u/re/ddoering/localGit/epix-hr-dev/firmware/simulations/CryoEncDec/sin.csv";
+  subtype word_t  is slv(DATA_BITS - 1 downto 0);
+  type    ram_t   is array(0 to DEPTH_C - 1) of word_t;
+
+  impure function readWaveFile(FileName : STRING) return ram_t is
+    file     FileHandle   : TEXT open READ_MODE is FileName;
+    variable CurrentLine  : LINE;
+    variable TempWord     : integer; --slv(DATA_BITS - 1 downto 0);
+    variable TempWordSlv  : slv(16 - 1 downto 0);
+    variable Result       : ram_t    := (others => (others => '0'));
+  begin
+    for i in 0 to DEPTH_C - 1 loop
+      exit when endfile(FileHandle);
+      readline(FileHandle, CurrentLine);
+      read(CurrentLine, TempWord);
+      TempWordSlv  := toSlv(TempWord, 16);
+      Result(i)    := TempWordSlv(15 downto 16 - DATA_BITS);
+    end loop;
+    return Result;
+  end function;
+
+  -- waveform signal
+  signal ramWaveform    : ram_t    := readWaveFile(FILENAME_C);
+  
+  -- component ports
+  signal sysClkRst : std_logic := '1';
+  signal idelayCtrlRdy : std_logic := '0';
+  signal dClkP : sl := '1'; -- Data clock
+  signal dClkN : sl := '0';
+  signal fClkP : sl := '0'; -- Frame clock
+  signal fClkN : sl := '1';
+  signal sDataP : sl;                       
+  signal sDataN : sl;
+  signal gearboxOffset : slv(2 downto 0) := "000";
+  signal bitSlip       : slv(3 downto 0) := "0101";
+  -- encoder
+  signal EncValidIn  : sl              := '1';
+  signal EncReadyIn  : sl;
+  signal EncDataIn   : slv(11 downto 0);
+  signal EncDispIn   : slv(1 downto 0) := "00";
+  signal EncDataKIn  : sl;
+  signal EncValidOut : sl;
+  signal EncReadyOut : sl              := '1';
+  signal EncDataOut  : slv(13 downto 0);
+  signal EncDispOut  : slv(1 downto 0);
+  signal EncSof      : sl := '0';
+  signal EncEof      : sl := '0';
+  signal DecValidOut : sl;
+  signal DecDataOut  : slv(11 downto 0);
+  signal DecDataKOut : sl;
+  signal DecDispOut  : slv(1 downto 0);
+  signal DecCodeError: sl;
+  signal DecDispError: sl;
+  signal DecSof      : sl;
+  signal DecEof      : sl;
+  signal DecEofe     : sl;
+  signal DecValid    : sl;
+
+  signal serialDataOut : sl;
+
+
+  -- axilite
+  signal axilWriteMaster : AxiLiteWriteMasterType;
+  signal axilWriteSlave  : AxiLiteWriteSlaveType;
+  signal axilReadMaster  : AxiLiteReadMasterType;
+  signal axilReadSlave   : AxiLiteReadSlaveType;
+  signal registerValue   : slv(31 downto 0);    
+  signal adcSerial       : Ad9249SerialGroupType;
+  signal adcStreams      : AxiStreamMasterArray(NUM_CHANNELS_G-1 downto 0);
+
+
+  -- clock
+  signal sysClk    : std_logic := '1';
+
+
+begin  --
+
+  U_encoder : entity work.SspEncoder12b14b 
+   generic map (
+     TPD_G          => TPD_G,
+     RST_POLARITY_G => '1',
+     RST_ASYNC_G    => false,
+     AUTO_FRAME_G   => true,
+     FLOW_CTRL_EN_G => false)
+   port map(
+      clk      => fClkP,
+      rst      => sysClkRst,
+      validIn  => EncValidIn,
+      readyIn  => EncReadyIn,
+      sof      => EncSof,
+      eof      => EncEof,
+      dataIn   => EncDataIn,
+      validOut => EncValidOut,
+      readyOut => EncReadyOut,
+      dataOut  => EncDataOut);
+
+  U_serializer :  entity work.serializerSim is
+    generic map(
+        g_dwidth => 14 
+    )
+    port map(
+        clk_i     => dClkP,
+        reset_n_i => not sysClkRst,
+        data_i    => EncDataOut,
+        data_o    => serialDataOut
+    );
+
+  -- DUT is the deserializer using iserdes3 for ultrascale devices
+  -- DUT enables data synchronization based on a channel data pattern or on frame clock.
+  
+  U_decoder : entity work.SspDecoder12b14b 
+    generic map(
+      TPD_G          => TPD_G,
+      RST_POLARITY_G => '1',
+      RST_ASYNC_G    => false)
+   port map(
+      clk       => fClkP,
+      rst       => sysClkRst,
+      validIn   => EncValidOut,
+      dataIn    => EncDataOut,
+      validOut  => DecValidOut,
+      dataOut   => DecDataOut,
+      valid     => DecValid,
+      sof       => DecSof,
+      eof       => DecEof,
+      eofe      => DecEofe,
+      codeError => DecCodeError,
+      dispError => DecDispError);
+  
+  
+  -- clock generation
+  sysClk <= not sysClk after 4 ns;
+  --
+  fClkP <= not fClkP after 7 * 4 ns;
+  fClkN <= not fClkP;
+  dClkP <= not dClkP after 4 ns; 
+  dClkN <= not dClkP;
+  --
+
+  EncDataIn_Proc: process
+    variable dataIndex : integer := 0;
+  begin
+    wait until fClkP = '1';
+    if EncValidIn = '1' then
+      EncDataIn <= ramWaveform(dataIndex);
+      dataIndex := dataIndex + 1;
+      if dataIndex = DEPTH_C then
+        dataIndex := 0;
+      end if;
+    else
+      EncDataIn <= IDLE_PATTERN_C;
+    end if;
+  end process;
+  
+  -- waveform generation
+  WaveGen_Proc: process
+    variable registerData    : slv(31 downto 0);  
+  begin
+
+    ---------------------------------------------------------------------------
+    -- reset
+    ---------------------------------------------------------------------------
+    wait until sysClk = '1';
+    sysClkRst  <= '1';
+    EncValidIn <= '0';
+
+    wait for 1 us;
+    sysClkRst <= '0';
+    idelayCtrlRdy <= '1';               -- simulates control ready signal
+            
+    wait for 10 us;
+    EncValidIn <= '1';                  -- starts sending realData
+
+    wait for 100 us;
+    EncValidIn <= '0';                  -- starts sending realData
+
+    wait for 10 us;
+    EncValidIn <= '1';                  -- starts sending realData
+
+    ---------------------------------------------------------------------------
+    -- load axilite registers
+    ---------------------------------------------------------------------------
+    --
+    wait until sysClk = '1';
+    -- change to axil register command
+    wait until sysClk = '0';
+    --loadDelay <= '0';
+    --axiLiteBusSimRead (sysClk, axilReadMaster, axilReadSlave, x"00000020", registerData, true);
+    registerValue <= registerData;
+    wait for 1 us;
+    
+    --axiLiteBusSimWrite (sysClk, axilWriteMaster, axilWriteSlave, x"00000020", x"00000F55", true);
+    wait for 10 us;    
+    
+    --axiLiteBusSimRead (sysClk, axilReadMaster, axilReadSlave, x"00000020", registerData, true);
+    registerValue <= registerData;
+    wait for 1 us;
+
+    ---------------------------------------------------------------------------
+    -- start gearbox offset search
+    ---------------------------------------------------------------------------
+    gearboxOffset <= "000";
+    bitSlip <= "0000";
+
+
+    wait for 10 us;
+    
+
+    
+    wait;
+  end process WaveGen_Proc;
+
+  
+
+end cryo_tb_arch;
+
