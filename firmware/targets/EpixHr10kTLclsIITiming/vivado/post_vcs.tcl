0c4b0c7d595f2db9ca8b7e7c532eb0e8deafe228
Larry Ruckman <ruckman@slac.stanford.edu>
2022-06-20 17:24:10 -0700

    adding post_vcs.tcl

---
+++ firmware/targets/EpixHr10kTLclsIITiming/vivado/post_vcs.tcl
@@ -0,0 +1,17 @@
+##############################################################################
+## This file is part of 'SLAC Firmware Standard Library'.
+## It is subject to the license terms in the LICENSE.txt file found in the
+## top-level directory of this distribution and at:
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+## No part of 'SLAC Firmware Standard Library', including this file,
+## may be copied, modified, propagated, or distributed except according to
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+
+# Get variables and procedures
+source -quiet $::env(RUCKUS_DIR)/vivado/env_var.tcl
+source -quiet $::env(RUCKUS_DIR)/vivado/proc.tcl
+
+set simTbOutDir ${OUT_DIR}/${PROJECT}_project.sim/sim_1/behav
+
+exec echo export PROJ_DIR = ${PROJ_DIR} >> ${simTbOutDir}/setup_env.sh
