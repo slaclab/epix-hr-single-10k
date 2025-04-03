d336ed5e9079790d1b63420fbf6b9a9cc1841ffe
Dionisio Doering <ddoering@slac.stanford.edu>
2025-04-03 14:16:13 -0700

    updating tools to s3df

--- firmware/setup_template.sh
+++ firmware/setup_template.sh
@@ -1,6 +1,10 @@
 # Setup the Xilinx software and licensing
 
-source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2022.2/Vivado/2022.2/settings64.sh
+#source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2022.2/Vivado/2022.2/settings64.sh
+source /sdf/group/faders/tools/xilinx/2024.2/Vivado/2024.2/settings64.sh
+
 # Setup the VCS software
-source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/T-2022.06-SP2/settings.sh
-source /afs/slac.stanford.edu/g/reseng/zeromq/4.2.0/settings.sh
+#source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/T-2022.06-SP2/settings.sh
+#source /afs/slac.stanford.edu/g/reseng/zeromq/4.2.0/settings.sh
+
+source /sdf/group/faders/tools/synopsys/vcs/W-2024.09/settings.sh
